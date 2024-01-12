a32ca34c9961988cdc18d9bdc112789cb521a611
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2024-01-12 21:00:55 +0100

    abababa

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
+use clap::Parser;
+use osu_db::collection::Collection;
+use osu_db::listing::Beatmap;
+use osu_db::{CollectionList, Listing, Mode};
+use rosu_pp::{BeatmapExt, PerformanceAttributes};
 use std::collections::HashMap;
 use std::path::Path;
-use clap::Parser;
-use osu_db::{CollectionList, Listing, Mode};
-use osu_db::collection::Collection;
-use rosu_pp::{BeatmapExt, PerformanceAttributes};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-/// Create osu! collections based on mapper
+/// Create osu! collections based on aim/tapping ratio
 struct Args {
     #[arg(default_value = ".")]
     /// Path to the osu! directory
     osu_path: String,
 
-    #[arg(short, long, default_value = "% ")]
+    #[arg(long, default_value = "% ")]
     /// The prefix to add to each collection
     collection_prefix: String,
 
-    #[arg(short, long, default_value = "5.0")]
+    #[arg(long, default_value = "5.0")]
     /// The multiples of which the aim ratio is grouped by (eg. precision 5 => groups of 50%, 55%, 60%...)
     ratio_precision: f64,
+
+    #[arg(long, default_value = "4.0")]
+    /// The minimum star rating to consider for collections (will speed up the process a lot)
+    min_star_rating: f64,
 }
 
 fn main() {
     let args = Args::parse();
 
+    println!("Starting process with following arguments: {:?}", args);
+
     let osu_path = Path::new(&args.osu_path);
 
     let db_path = osu_path.join("osu!.db");
-    if !db_path.exists() { panic!("osu!.db was not found"); }
+    if !db_path.exists() {
+        panic!("osu!.db was not found");
+    }
 
     let collection_path = osu_path.join("collection.db");
-    if !collection_path.exists() { panic!("collection.db was not found"); }
+    if !collection_path.exists() {
+        panic!("collection.db was not found");
+    }
 
     println!("Reading osu!.db");
+
     let listing = Listing::from_file(&db_path).expect("Could not read osu!.db");
     let aim_ratio_groups = group_maps_by(&args, listing);
 
@@ -45,11 +57,35 @@ fn main() {
     add_new_collections(&args, aim_ratio_groups, &mut collections);
 
     collections.to_file(collection_path).unwrap();
+
+    println!("Successfully wrote collection.db");
 }
 
 fn group_maps_by(args: &Args, listing: Listing) -> HashMap<i32, Vec<Option<String>>> {
-    listing.beatmaps.iter()
-        .filter(|map| map.mode == Mode::Standard)
+    let filtered_maps: Vec<&Beatmap> = listing
+        .beatmaps
+        .iter()
+        .filter(|map| {
+            map.mode == Mode::Standard
+                && map
+                    .std_ratings
+                    .iter()
+                    .find_map(|(mods, stars)| if mods.0 == 0 { Some(stars) } else { None })
+                    .unwrap_or(&args.min_star_rating)
+                    >= &args.min_star_rating
+        })
+        .collect();
+
+    println!(
+        "Starting processing on {} out of {} total maps...",
+        filtered_maps.len(),
+        listing.beatmaps.len()
+    );
+
+    let mut count = 0;
+
+    filtered_maps
+        .iter()
         .fold(HashMap::new(), |mut hash_map, map| {
             let map_path = Path::new(&args.osu_path)
                 .join("Songs")
@@ -66,24 +102,33 @@ fn group_maps_by(args: &Args, listing: Listing) -> HashMap<i32, Vec<Option<Strin
 
             if let PerformanceAttributes::Osu(pp) = map_pp.pp().accuracy(99f64).calculate() {
                 let aim_aspect = pp.pp_aim / (pp.pp_aim + pp.pp_speed);
-                let rounded_aim_aspect = ((aim_aspect * 100f64 / args.ratio_precision).floor() * args.ratio_precision) as i32; // to multiples of 5
+                let rounded_aim_aspect = ((aim_aspect * 100f64 / args.ratio_precision).floor()
+                    * args.ratio_precision) as i32;
 
                 hash_map
                     .entry(rounded_aim_aspect)
-                    .or_insert(Vec::new())
+                    .or_default()
                     .push(map.hash.clone());
+            }
+
+            count += 1;
+
+            if count % 100 == 0 {
+                println!("Processed {}/{} maps", count, filtered_maps.len());
             }
 
             hash_map
         })
 }
 
-fn add_new_collections(args: &Args, aim_ratio_groups: HashMap<i32, Vec<Option<String>>>, collections: &mut CollectionList) {
-    let mut count = 0;
-
+fn add_new_collections(
+    args: &Args,
+    aim_ratio_groups: HashMap<i32, Vec<Option<String>>>,
+    collections: &mut CollectionList,
+) {
     for (aim_ratio, maps) in aim_ratio_groups {
         let prefix = &args.collection_prefix;
-        let collection_name = format!("{prefix}{aim_ratio}% Aim / {}% Speed", 100 - aim_ratio);
+        let collection_name = format!("{prefix}{aim_ratio}% Aim / {}% Tapping", 100 - aim_ratio);
 
         println!("Adding {collection_name} with {} maps", maps.len());
 
@@ -91,11 +136,7 @@ fn add_new_collections(args: &Args, aim_ratio_groups: HashMap<i32, Vec<Option<St
             name: Some(collection_name),
             beatmap_hashes: maps,
         });
-
-        count += 1;
     }
-
-    println!("Successfully wrote {} collections", count);
 }
 
 fn remove_previous_collections(args: &Args, collections: &mut CollectionList) {
@@ -107,5 +148,9 @@ fn remove_previous_collections(args: &Args, collections: &mut CollectionList) {
             true
         }
     });
-    println!("Removed {} collections from previous iteration", collection_count - collections.collections.len());
+
+    println!(
+        "Removed {} collections from previous iteration",
+        collection_count - collections.collections.len()
+    );
 }
